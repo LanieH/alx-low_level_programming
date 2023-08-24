c38837e61fc0054c0fdc85ace81be5581c3cbd2f
LanieH <honourssithole@gmail.com>
2023-08-24 16:28:31 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -30,7 +30,7 @@ void rev_string(char *n)
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
- * size_r: buffer size
+ * @size_r: buffer size
  * Return: pointer to calling function
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
