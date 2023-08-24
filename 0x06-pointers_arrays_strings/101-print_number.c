facdaabb36b5e02b4af6a72592a804c1ca064701
LanieH <honourssithole@gmail.com>
2023-08-24 14:24:03 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,36 +2,25 @@
 
 /**
  * print_number - print number
- * @n: number to print
+ * @n: integer params
+ *
+ * Return: 0
  */
 void print_number(int n)
 {
-	unsigned int tens, unit, positive_value = n;
-	double tens_value = 1;
+	unsigned int n1;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		if (n < 0)
-		{
-			positive_value = (n * -1);
-			_putchar('-');
-		}
-		while (tens_value <= positive_value)
-		{
-			tens_value *= 10;
-		}
-		tens = tens_value / 10;
+	n1 = n;
 
-		while (ten >= 1)
-		{
-			unit = positive_value / tens;
-			_putchar(unit + '0');
-			positive_value = (positive_value - (tens * unit));
-			tens /= 10;
-		}
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
 	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
