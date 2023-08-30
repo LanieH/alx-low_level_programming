20dbf155c90686a11b8e73504cc647bd3070f27d
LanieH <honourssithole@gmail.com>
2023-08-30 15:54:49 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,66 @@
 #include "main.h"
 
 /**
- * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
- * Return: 0
+ * check_sub_match - checks if the substring after * matches with s1
+ * @s1: base string
+ * @s2: string being compared
+ * @place: placeholder for index just speacial char (*)
+ * Return: 1 if match, 0 if not
  */
-int wildcmp(char *s1, char *s2)
+int check_sub_match(char *s1, char *s2, char *place)
 {
-	if (*s1 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s == '\0');
+		return (1);
+	}
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (check_sub_match(s1, s2 + 1, s2 + 1));
+	}
+
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (0);
 	}
 
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		return (check_sub_match(s1, s2 + 1, s2 + 1));
 	}
-	else if (*s1 == *s2)
+
+	if (*s1 == *s2)
+	{
+		return (check_sub_match(s1 + 1, s2 + 1, place));
+	}
+
+	else
+	{
+		return (check_sub_match(s1 + 1, place, place));
+	}
+}
+
+/**
+ * wildcmp - compares 2 strings, one with special char '*'
+ * @s1: base string
+ * @s2: string being compared
+ * Return: '1' if match, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	return (0);
+
+	else if (*s2 == '*')
+	{
+		return (check_sub_match(s1, (s2 + 1), (s2 + 1)));
+	}
+	else
+		return (0);
 }
