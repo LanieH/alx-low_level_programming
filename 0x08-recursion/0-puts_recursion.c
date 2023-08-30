26c1e5410bba273cd2f9b146f25cc5f796fbeae3
LanieH <honourssithole@gmail.com>
2023-08-30 11:32:40 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (successful)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + s);
+	}
+	else
+		_putchar('\n');
+}
