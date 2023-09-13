2968722f9182119eb8391bd9e0c8292ff6ba1ac5
LanieH <honourssithole@gmail.com>
2023-09-13 16:38:59 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,8 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,17 +18,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	operation = get_op_func(argv[2]);
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	result = (*operation)(num1, num2);
-
-	printf("%d\n", result);
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
