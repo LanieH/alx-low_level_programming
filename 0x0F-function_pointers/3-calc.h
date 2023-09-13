176f392c9c855aac1a5bba7c0644bd24dacc6fb0
LanieH <honourssithole@gmail.com>
2023-09-13 15:31:39 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,9 +2,10 @@
 #define CALC_H
 
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
