deed02ea295fedae67dc1279744804654605d962
LanieH <honourssithole@gmail.com>
2023-09-26 16:14:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
-	for (i = 0; temp != NULL && i < idex - 1; i++)
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 		temp = temp->next;
 
 	if (temp == NULL)
